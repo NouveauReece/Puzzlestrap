43e064d70b20676700f5cbd8b4b8c0f41761a6e6
NouveauReece <nouveaureece@protonmail.com>
2023-12-01 22:41:06 -0500

    🎈 Added Modals

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -27,6 +27,7 @@
   }
 
   const Popper__namespace = /*#__PURE__*/_interopNamespaceDefault(Popper);
+  
 
   /**
    * --------------------------------------------------------------------------
@@ -4492,3 +4493,24 @@
 
 }));
 //# sourceMappingURL=bootstrap.js.map
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
